eca2be5f8fecb50674c7ec7f0e12cd9610ef028b
wataru063 <skgch0702@gmail.com>
2024-03-17 21:08:35 +0900

    add handling to call qiita api

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -21,4 +21,25 @@ class _SearchScreenState extends State<SearchScreen> {
       body: Container(),
     );
   }
+
+  Future<List<Article>> searchQiita(String keyword) async {
+    // 1. http通信に必要なデータを準備をする
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title$keyword',
+      'per_page': '10',
+    });
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+    // 2. Qiita APIにリクエストを送る
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Beare $token',
+    });
+    if (res.statusCode == 200) {
+      // 3. 戻り値をArticleクラスの配列に変換
+      final List<dynamic> body = jsonDecode(res.body)
+      // 4. 変換したArticleクラスの配列を返す(returnする)
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
+  }
 }
